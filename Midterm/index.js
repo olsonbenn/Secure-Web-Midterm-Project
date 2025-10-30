bd0b918bc3140d53ad84ac64a2d16cf61a070698
olsonbenn <olsonbenn@uwplatt.edu>
2025-10-29 23:39:13 -0500

    fix again

--- Midterm/index.js
+++ Midterm/index.js
@@ -4,12 +4,12 @@ function loadData(data){
         div.classList.add("col-sm-6", "col-md-4", "col-lg-3");
         div.innerHTML = `
         <div class="card mb-4" style="width: 100%;">
-            <img src=${game.image_url} class="card-img-top" alt="image of board game" width="100" />
+            <img src=${game.imageurl} class="card-img-top" alt="image of board game" width="100" />
             <div class="card-body">
                 <h5 class="card-title">${game.title}</h5>
                 <p class="card-text">
                     <strong>Year:</strong> ${game.year}
-                    Wikipedia Page: ${game.wiki_url}
+                    Wikipedia Page: ${game.wikiurl}
                 </p>
             </div>
         </div>
